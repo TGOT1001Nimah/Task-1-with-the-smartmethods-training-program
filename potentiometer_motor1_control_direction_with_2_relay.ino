96d8771f009e002b31f9fb53db38fe955f4c623b
Nimah Alyamani <66944123+TGOT1001Nimah@users.noreply.github.com>
2020-07-13 03:30:42 +0300

    this is the for task 2 dc motor with 2 relay
    her the task 2 controlling the direction in DC motor by using 2 relay

---
+++ potentiometer_motor1_control_direction_with_2_relay.ino
@@ -0,0 +1,35 @@
+
+int sensorValue = 0;//this is for potentiometer
+int potvalue = 125;//a value for potentiometer depending on it is resistration which is half 255 komh
+
+void setup()
+{
+  pinMode(A0, INPUT);//define the potentiometer as input
+  pinMode(4, OUTPUT);//difine the pin in relay 1 for right direction
+  pinMode(5, OUTPUT);//difine the pin in relay 2 for left direction
+}
+
+void loop()
+{
+  sensorValue = analogRead(A0);//her it will takes the value from A0 pin 
+  Serial.print(sensorValue);//it show you the value from A0 to let you know if less or more than 125
+  if (sensorValue == potvalue)//her if the value equal 125 poth relay pin will be open
+  {
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+  }
+ else
+  {
+  	if (sensorValue < potvalue) //but if the value less than 125 the motor will be in right direction
+  	{
+    	digitalWrite(4, LOW);
+      digitalWrite(5, HIGH);
+  	}
+  	else ////els mean  if it is  more than 125 the motor will be in left direction
+  	{
+    	digitalWrite(4, HIGH);
+      digitalWrite(5, LOW);
+  	}
+  }
+}
+  
