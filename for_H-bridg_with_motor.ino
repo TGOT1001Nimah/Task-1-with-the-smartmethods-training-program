269566d17d7e14aba5d2fb2008d8beb648b59378
Nimah Alyamani <66944123+TGOT1001Nimah@users.noreply.github.com>
2020-08-07 00:52:59 +0300

    task 5 for training program smart-method
    made H-bridge with at least 3-4 A

---
+++ for_H-bridg_with_motor.ino
@@ -0,0 +1,23 @@
+
+int left =8; //this is connected with npn 
+int right =7;//the same like before but with the other npn
+
+void setup()
+{
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop()
+{
+   // Wait for 1000 millisecond(s)
+ 
+  digitalWrite(7, LOW);//the right directhon will off
+  digitalWrite(8, HIGH);//the left direction on
+  delay(2000); // Wait for 2000 millisecond
+  digitalWrite(8, LOW);//then the right direction on 
+  digitalWrite(7, HIGH);//left direction of
+  delay(2000);//
+  
+  
+}
