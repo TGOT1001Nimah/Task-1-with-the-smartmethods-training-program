e8c3d0ac0b189fcbf64b34f39a8c8c32359e227d
Nimah Alyamani <66944123+TGOT1001Nimah@users.noreply.github.com>
2020-08-09 02:26:50 +0300

    task 1 stepper motor
    this is for training program with smart-method

---
+++ stepper.ino
@@ -0,0 +1,20 @@
+#include <Stepper.h>
+int spr = 20;//stepper steps
+int D=0;
+int x=0;
+int c1= 10;//control one
+int c2= 11;//control tow
+Stepper stepper(spr, 3, 4, 8, 9);
+
+void setup() {
+  // put your setup code here, to run once:
+stepper.setSpeed(60);//set the speed of the motor to 60 RPMs
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+stepper.step(5);//right direction
+delay(5000);
+stepper.step(-5);//to left direction
+delay(5000);
+}
